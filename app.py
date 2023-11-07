b415a7a8ac00e7f101f0d673797f6552fe8cc843
ayush yajnik <ayush.yajnik@yahoo.com>
2023-11-07 11:15:56 -0500

    azure storage blob upgraded

--- app.py
+++ app.py
@@ -1,53 +1,48 @@
 import os
-from azure.storage.filedatalake import (
-    DataLakeServiceClient,
-    DataLakeDirectoryClient,
-    FileSystemClient
-)
+# from azure.storage.filedatalake import (
+#     DataLakeServiceClient,
+#     DataLakeDirectoryClient,
+#     FileSystemClient
+# )
 from azure.identity import DefaultAzureCredential
 import azure.storage.blob.blockblobservice
 import datetime
 from datetime import datetime
 import pandas as pd
+from datetime import datetime, timedelta
+#from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions
+import pandas as pd
+from azure.storage.blob import BlobServiceClient
 
-## resource group: tokyo-olympic-analysis
+# account_name = 'tokyoolympicsanalysis'
+# account_key = 'Q/2Up2Id9usdnnic3RaiUQw/p98nR+DTqoDXKaa6JD950Ba4UNWGb30+iNVPczhsULhSa9kUTkx6+AStKmiPCw=='
+# container_name = 'containerrawdata'
 
-## storage account: tokyoolympicsayush
+# #create a client to interact with blob storage
+# connect_str = 'DefaultEndpointsProtocol=https;AccountName=' + account_name + ';AccountKey=' + account_key + ';EndpointSuffix=core.windows.net'
+# blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
-## Step 1:
+# #use the client to connect to the container
+# container_client = blob_service_client.get_container_client(container_name)
 
-# To work with the code examples, we need to create an authorized DataLakeServiceClient instance that represents the storage account. 
-# We can authorize a DataLakeServiceClient object using Microsoft Entra ID, an account access key, or a shared access signature (SAS).
- 
-## access key: qBCN/PQ1cTuD/sgVyNVosaFt+RavmhnVY4kYw/MUNmXlWOluXwlBJJ8cEkLIcmNZ9YkADtMSXULJ+AStZuuJHw==
-## account name: tokyoolympicsayush
-account_url = ""
-service_client = DataLakeServiceClient(account_url, credential='')
-
-# Step 2: Create a container
-
-# A container acts as a file system for your files. You can create a container by using the following method:
-
-# make sure that the name of the container can only contain the following characters
-# 1. lower case alphabets
-# 2. numbers
-# 3. hyphens
-
-#file_system_client = service_client.create_file_system('tokyorawdata1')
-
-# create a directory
+# #get a list of all blob files in the container
+# blob_list = []
+# for blob_i in container_client.list_blobs():
+#     blob_list.append(blob_i.name)
+    
 
 
-
-# reading a csv file from pandas which is in a container
-# Instantiate a DataLakeServiceClient using a connection string
-
-
-
-
-
-
-
-
-
-
+# df_list = []
+# #generate a shared access signiture for files and load them into Python
+# for blob_i in blob_list:
+#     #generate a shared access signature for each blob file
+#     sas_i = generate_blob_sas(account_name = account_name,
+#                                 container_name = container_name,
+#                                 blob_name = blob_i,
+#                                 account_key=account_key,
+#                                 permission=BlobSasPermissions(read=True),
+#                                 expiry=datetime.utcnow() + timedelta(hours=1))
+    
+#     sas_url = 'https://' + account_name+'.blob.core.windows.net/' + container_name + '/' + blob_i + '?' + sas_i
+    
+# df = pd.read_csv(sas_url)
