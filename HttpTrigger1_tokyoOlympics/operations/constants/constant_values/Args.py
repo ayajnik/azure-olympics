1300e01478b3dddd7cdff6b448638b44cce7eb2d
ayush yajnik <ayush.yajnik@yahoo.com>
2023-11-10 15:26:46 -0500

    removing creds

--- HttpTrigger1_tokyoOlympics/operations/constants/constant_values/Args.py
+++ HttpTrigger1_tokyoOlympics/operations/constants/constant_values/Args.py
@@ -5,6 +5,6 @@ sys.path.append('..')
 
 @unique
 class constant(Enum):
-    account_name = 'containerrawdata'
-    account_key = '/DfOVOYkvpsfaZVv9u56Tewuokj6h25eJ0yWTlxgR4Top3rl+6tFc5DKjIFLOcaxOVLieRodyWDu+ASt7W2Jmg=='
+    account_name = ''
+    account_key = ''
     container_name = 'containerrawdata1'
