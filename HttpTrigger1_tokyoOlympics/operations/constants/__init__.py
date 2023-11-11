b4a4561bb4ea6b21f5e05b3b52ac75134d50217c
ayush yajnik <ayush.yajnik@yahoo.com>
2023-11-11 15:51:38 -0500

    adding argument parser

--- HttpTrigger1_tokyoOlympics/operations/constants/__init__.py
+++ HttpTrigger1_tokyoOlympics/operations/constants/__init__.py
@@ -1,9 +1,30 @@
 try:
     from HttpTrigger1_tokyoOlympics.operations.constants.constant_values.Args import constant
+    import argparse
+    import sys
+
     print("Import constant values")
 except ImportError as e:
     print(e)
 
-name = constant.account_name.value
-key = constant.account_key.value
-container = constant.container_name.value
+args = None
+
+def parse_args():
+    global args
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--{}'.format(constant.account_name.value),'-accnt_name',help="Account name for Microsoft Azure")
+    parser.add_argument('--{}'.format(constant.account_key.value),'-accnt_key',help="Access keys for Storage account in Microsoft Azure")
+    parser.add_argument('--{}'.format(constant.container_name.value),'-cont_name',help="Container Name in Microsoft Azure Storage account")
+    args = vars(parser.parse_args())
+
+    return args
+
+def get_run_args(key):
+    parse_args()
+    return args[key]
+
+name = get_run_args(constant.account_name.value)
+key = get_run_args(constant.account_key.value)
+container = get_run_args(constant.container_name.value)
--- HttpTrigger1_tokyoOlympics/operations/constants/constant_values/Args.py
+++ HttpTrigger1_tokyoOlympics/operations/constants/constant_values/Args.py
@@ -5,6 +5,6 @@ sys.path.append('..')
 
 @unique
 class constant(Enum):
-    account_name = 'containerrawdata'
-    account_key = '/DfOVOYkvpsfaZVv9u56Tewuokj6h25eJ0yWTlxgR4Top3rl+6tFc5DKjIFLOcaxOVLieRodyWDu+ASt7W2Jmg=='
-    container_name = 'containerrawdata1'
+    account_name = 'account_name'
+    account_key = 'account_key'
+    container_name = 'container_name'
